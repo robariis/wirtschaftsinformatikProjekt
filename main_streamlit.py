8e745160b13d1cd780758380f1dd3abefcdcd406
robariis <63434705+robariis@users.noreply.github.com>
2024-04-24 23:19:22 +0200

    Update main_streamlit.py

--- main_streamlit.py
+++ main_streamlit.py
@@ -1,4 +1,6 @@
+import yfinance as yf
 import streamlit as st
+import pandas as pd
 
 st.write(
     """
@@ -6,3 +8,10 @@ st.write(
     Hello *world!*
     """
 )
+
+tickerSymbol = 'GOOGL'
+tickerData = yf.Ticker(tickerSymbol)
+tickerDf = tickerData.history(period='id', start='2010-5-31', end='2020-5-31')
+
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume)
